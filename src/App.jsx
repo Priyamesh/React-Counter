00be6845c30ec8a596ce2013a45afb5fefafe835
priyamesh <priyamesh28@gmail.com>
2023-12-27 19:11:16 +0530

    incr by 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
     else alert("You have reached the Maximum limit");
   };
 
+  const incrCounter2 = () => {
+    if (counter < 99) {
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+    } else alert("You have reached the Maximum limit");
+  };
+
   const decrCounter = () => {
     if (counter > 0) setCounter(counter - 1);
     else alert("You have recahed the Minimum limit");
@@ -19,6 +26,7 @@ function App() {
       <h1>React Counter Project</h1>
       <h2>Counter : {counter}</h2>
       <button onClick={incrCounter}> Increase Counter </button>
+      <button onClick={incrCounter2}> Increase Counter by 2 </button>
       <button onClick={decrCounter}> Decrease Counter </button>
     </>
   );
